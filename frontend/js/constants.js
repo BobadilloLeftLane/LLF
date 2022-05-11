339ab4a09245c50785952ba798215ce43087361c
BobadilloLeftLane <nikolabogdanic7@gmail.com>
2022-05-11 19:46:56 +0200

    css changes

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,8 +1,8 @@
 const contractAddress = "0x1445E8bdFDbec1fe78Cf186bD5361e645a4F6abD"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
+const welcome_h1 = "Welcome to the LLF NFT Project!!";
+const welcome_h2 = "Connect to MetaMask wallet to get started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
