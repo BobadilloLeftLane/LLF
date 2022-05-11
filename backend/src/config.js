95a9d818f2d2ce50ca48e045fffc7f0c02ee35f2
BobadilloLeftLane <nikolabogdanic7@gmail.com>
2022-05-11 19:33:59 +0200

    css change

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x1445E8bdFDbec1fe78Cf186bD5361e645a4F6abD"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
